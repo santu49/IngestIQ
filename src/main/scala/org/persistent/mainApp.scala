2dd8dc771a41b7b495890da368ad226b2ba6c91c
santuroy <santu_roy@persistent.com>
2023-04-06 19:12:33 +0530

    started work on csv to csv modified data

--- src/main/scala/org/persistent/mainApp.scala
+++ src/main/scala/org/persistent/mainApp.scala
@@ -2,6 +2,8 @@ package org.persistent
 
 import org.apache.spark.sql.SparkSession
 
+import scala.io.StdIn.readLine
+
 object mainApp {
 
   def createSparkSession(): SparkSession = {
@@ -26,30 +28,55 @@ object mainApp {
     var configFileData = spark.emptyDataFrame;
     if (check == "Success") {
       configFileData = connectionOBJ.readConfigFile(config_Json_file_path, file_type);
-//      configFileData.show();
+      //      configFileData.show();
       val srcConnectionData = connectionOBJ.getSrcConnections(configFileData);
-      //      srcConnectionData.show()
+//      srcConnectionData.show()
       val tarConnectionData = connectionOBJ.getTarConnections(configFileData);
-      //      tarConnectionData.show()
+//      tarConnectionData.show()
       import spark.implicits._
       val srcConnectionTypeData = srcConnectionData.select("connectionType").distinct()
         .map(f => f.getString(0)).collect().toList;
-      //      println(srcConnectionTypeData)
+      //            println(srcConnectionTypeData)
       val tarConnectionTypeData = tarConnectionData.select("connectionType").distinct()
         .map(f => f.getString(0)).collect().toList;
       //      print(tarConnectionTypeData)
 
+
       var srcFileData = spark.emptyDataFrame
-      //for source
+
+      //---------------------csv to csv----------------------
+      if (srcConnectionTypeData(0) == "fileSystem" && tarConnectionTypeData(0) == "fileSystem") {
+        val srcConnectionFileType = configFileData.filter(configFileData("type") === "source").select("fileType").distinct()
+          .map(f => f.getString(0)).collect().toList;
+//        print(srcConnectionFileType)
+        val tarConnectionFileType = configFileData.filter(configFileData("type") === "target").select("fileType").distinct()
+          .map(f => f.getString(0)).collect().toList;
+//        print(tarConnectionFileType)
+        if (srcConnectionFileType(0) == "csv" && tarConnectionFileType(0) == "csv") {
+          srcFileData = readDataOBJ.getDataFromFile(configFileData);
+          println("Do you want to load same data? (yes/no)")
+          val res = readLine
+          if (res == "No" || res == "no" || res == "NO") {
+            print("getting res")
+            sys.exit
+          }
+          //          val message = loadDataOBJ.putDataInLocalFile(configFileData, srcFileData);
+          //          println(message);
+        }
+
+      }
+      // ----------------for source----------------------
       if (srcConnectionTypeData(0) == "fileSystem") {
         srcFileData = readDataOBJ.getDataFromFile(configFileData);
-//        srcFileData.show(10);
+        //        srcFileData.show(10);
         //          write here
       } else if (srcConnectionTypeData(0) == "postgreSQL") {
         srcFileData = readDataOBJ.getDataFromPostGre(configFileData);
         //        srcFileData.show(10);
       }
-      // for target
+
+
+      //----------------------- for target---------------------
       if (tarConnectionTypeData(0) == "postgreSQL") {
         val message = loadDataOBJ.putDataInPostgreSQL(configFileData, srcFileData);
         println(message);
