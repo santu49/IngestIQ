47e5f274037e95d62c9246eaea57f1110a7d225a
santuroy <santu_roy@persistent.com>
2023-04-11 19:15:13 +0530

    minor changes

--- src/main/scala/org/persistent/loadData.scala
+++ src/main/scala/org/persistent/loadData.scala
@@ -6,6 +6,7 @@ import org.persistent.mainApp.createSparkSession
 import java.sql.DriverManager
 import java.util.Properties
 import scala.io.StdIn.readLine
+import scala.reflect.internal.util.NoFile.input
 
 class loadData {
 
@@ -108,24 +109,31 @@ class loadData {
     val tar_file_type = df.select("fileType").distinct().map(f => f.getString(0)).collect().toList(0)
     //      val src_file_data = spark.read.format(tar_file_type).option("header", "true").load(tar_file_path)
     //  srcFileData.show()
-
     println("What are you want to change: ")
     println("1. Changes in rows")
     println("2. Changes in column")
     val res = readLine
     if (res == "1") {
       println("Enter number of rows you want: ")
-      val rows = readLine();
-      val modifiedData = src_file_data.limit(rows.toInt)
+      val rows = readLine().toInt;
+      val modifiedData = src_file_data.limit(rows)
       modifiedData.coalesce(1).write.format(tar_file_type).option("header", "true").option("inferSchema", "true").save(tar_file_path)
       //      modifiedData.show()
     } else if (res == "2") {
-      
+//      val columns_count = readLine("Enter the number of columns you want: ").toInt
+//      val col_array = new Array[String](columns_count)
+//      println("Enter columns names: ")
+//      for (i <- 0 until columns_count) {
+//        col_array(i) = readLine
+//      }
+//      println("The array is: " + col_array.mkString(", "))
+
+      val column_names = input("Enter the column names you want to read (separated by commas): ").split(',')
 
 
     }
-        return "Successfully loaded Data"
-//    return "Hello from modified data"
+    return "Successfully loaded Data"
+    //    return "Hello from modified data"
 
   }
 
