254768244e3047f4eb813d7fae0634cc2c8781cf
santuroy <santu_roy@persistent.com>
2023-04-20 20:24:20 +0530

    added validator for src connector if file path and type is given wrong in config file

--- src/main/scala/org/persistent/connection.scala
+++ src/main/scala/org/persistent/connection.scala
@@ -2,6 +2,7 @@ package org.persistent
 
 import org.persistent.mainApp.createSparkSession
 import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
+import org.json4s.scalap.Failure.error
 
 import java.io.File
 
@@ -29,10 +30,30 @@ class connection {
     val Json_fileData = spark.read.format(file_type).option("multiline", true).load(Json_file_path)
     return Json_fileData
   }
+
   def getSrcConnections(configFileData: DataFrame): DataFrame = {
-    val src_ct_df = configFileData.filter(configFileData("type") === "source").select("connectionType")
+    val spark = createSparkSession()
+    var src_ct_df = spark.emptyDataFrame
+    import spark.implicits._
+    val srcConnectionTypeData = configFileData.filter(configFileData("type") === "source").select("connectionType").distinct()
+      .map(f => f.getString(0)).collect().toList
+    if (srcConnectionTypeData(0).toLowerCase == "filesystem") {
+      val df = configFileData.filter(configFileData("type") === "source").select("filePath", "fileType")
+      val src_file_path = df.select("filePath").distinct().map(f => f.getString(0)).collect().toList(0)
+      val src_file_type = df.select("fileType").distinct().map(f => f.getString(0)).collect().toList(0)
+      val file = new File(src_file_path)
+      if (file.exists() && file.isFile() && src_file_type == "csv" || src_file_type == "parquet") {
+        src_ct_df = configFileData.filter(configFileData("type") === "source").select("connectionType")
+      }
+      else {
+        throw new Exception("Error: Invalid file path or file type")
+      }
+
+
+    }
     return src_ct_df
   }
+
   def getTarConnections(configFileData: DataFrame): DataFrame = {
     val tar_ct_df = configFileData.filter(configFileData("type") === "target").select("connectionType")
     return tar_ct_df
